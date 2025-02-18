2be1111629efe9f35b630bc70ff97fb30877bad2
bensbits91 <benbpnw@gmail.com>
2025-02-18 06:49:29 -0800

    hide secrets

--- app/api/user-metadata/route.js
+++ app/api/user-metadata/route.js
@@ -8,16 +8,16 @@ export async function GET(req, res) {
   const userId = url.searchParams.get('userId');
   // console.log('bb ~ route.js ~ userId:', userId);
   try { // todo: get stuff from process.env
-    const tokenResponse = await axios.post('https://dev-zrsl41x0hrcrqlvn.us.auth0.com/oauth/token', {
-      client_id: 'tYJRNiX0mkBnNvzdo22fbeWNQ06ZQ5N3',
-      client_secret: 'vDYk9S-7lfh5cIFE0hmBBrQritH9J7Loj7klxblqpn0b_3BvlDNKwSKNPLXoDCpG',
+    const tokenResponse = await axios.post('https://*******.us.auth0.com/oauth/token', {
+      client_id: '*******',
+      client_secret: '*******',
       audience: 'https://things/api',
       grant_type: 'client_credentials'
     });
 
     const token = tokenResponse.data.access_token;
 
-    const userResponse = await axios.get(`https://dev-zrsl41x0hrcrqlvn.us.auth0.com/api/v2/users/${userId}`, {
+    const userResponse = await axios.get(`https://*******.us.auth0.com/api/v2/users/${userId}`, {
       headers: {
         Authorization: `Bearer ${token}`
       }
